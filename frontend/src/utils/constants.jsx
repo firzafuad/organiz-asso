3a4160ef81d7bb479e2534f495553546cd96f2e9
firzafuad <firzaachmad31@gmail.com>
2026-06-01 21:22:00 +0200

    move api variable to constants.jsx

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,1 +1,7 @@
+import axios from "axios";
+
 export const BACK_URI = import.meta.env.BACK_URI || "http://localhost:8000";
+export const api = axios.create({
+  baseURL: BACK_URI,
+  withCredentials: true
+});
