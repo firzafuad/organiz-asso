8049b3b091787477ee68bd75ae3d4eb31540ae48
firzafuad <firzaachmad31@gmail.com>
2026-06-02 19:08:36 +0200

    max to current date

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import "../styles/Filters.css";
 
 function Filters(props) {
+    const date = new Date()
+    const y = date.getFullYear()
+    const m = String(date.getMonth() + 1).padStart(2, '0')
+    const d = String(date.getDate()).padStart(2, '0')
+    const currDate = `${y}-${m}-${d}`
+
     const [debut, setDebut] = useState("");
-    const [fin, setFin] = useState("");
+    const [fin, setFin] = useState(currDate);
 
     const handleDebut = (evt) => {
         setDebut(evt.target.value);
@@ -12,6 +18,7 @@ function Filters(props) {
 
     const handleFin = (evt) => {
         setFin(evt.target.value);
+        console.log(currDate);
         props.onFilter("dateFin", evt.target.value);
     }
 
@@ -19,7 +26,7 @@ function Filters(props) {
         <label htmlFor="date_deb">Date de début</label>
         <input type="date" id="date_deb" value={debut} max={fin} onChange={handleDebut} />
         <label htmlFor="date_fin">Date de fin</label>
-        <input type="date" id="date_fin" value={fin} min={debut} onChange={handleFin} />
+        <input type="date" id="date_fin" value={fin} min={debut} max={currDate} onChange={handleFin} />
     </div>
 }
 
