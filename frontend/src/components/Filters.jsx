d5eaec9f94a7e399094a97edf39b5e715800dca9
firzafuad <firzaachmad31@gmail.com>
2026-06-02 18:51:04 +0200

    min max date

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "../styles/Filters.css";
 
 function Filters(props) {
+    const [debut, setDebut] = useState("");
+    const [fin, setFin] = useState("");
+
+    const handleDebut = (evt) => {
+        setDebut(evt.target.value);
+        props.onFilter("dateDebut", evt.target.value);
+    }
+
+    const handleFin = (evt) => {
+        setFin(evt.target.value);
+        props.onFilter("dateDebut", evt.target.value);
+    }
+
     return <div id="filtre_div">
         <label htmlFor="date_deb">Date de début</label>
-        <input type="date" id="date_deb" onChange={(evt) => props.onFilter("dateDebut", evt.target.value)} />
+        <input type="date" id="date_deb" value={debut} max={fin} onChange={handleDebut} />
         <label htmlFor="date_fin">Date de fin</label>
-        <input type="date" id="date_fin" onChange={(evt) => props.onFilter("dateFin", evt.target.value)} />
+        <input type="date" id="date_fin" value={fin} min={debut} onChange={handleFin} />
     </div>
 }
 
